05cb299bc702cdd52914913484b78c977b3f8919
pablo-fdr <pablodelrio.pf@gmail.com>
2024-10-06 23:39:33 -0300

    Subiendo mi landing page

---
+++ smoothscrowlling.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    links.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetID = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetID);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
